b1337b06425eb671c0ea043db64e9ddf960506a7
Erick Quintao <erickquintao@outlook.com>
2023-05-14 20:58:44 -0300

    começando

---
+++ merkava-versao2/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './Home';
+function App() {
+  return (
+    
+    <div className="App">
+    <Home/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ merkava-versao2/src/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './home/header/header';
+import Slider from './home/slides/slider';
+function Home (){
+ return(
+<div>
+    <Header/>
+    <Slider/>
+</div>
+ );
+}
+export default Home;
